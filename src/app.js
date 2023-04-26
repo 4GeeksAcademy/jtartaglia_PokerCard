48bea9660ac908120ee666b73ca6e5fdc3232b49
julitar <126813769+julitar@users.noreply.github.com>
2023-04-26 19:32:18 +0000

    Código JS sin color diseño carta básico

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = ["♦", "♥", "♠", "♣"];
+  const valores = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const createCard = () => {
+    let palosRandom = palos[Math.floor(Math.random() * palos.length)];
+    let valoresRandom = valores[Math.floor(Math.random() * valores.length)];
+
+    let paloSuperior = document.querySelector("#palo-superior");
+    paloSuperior.innerHTML = palosRandom;
+
+    let paloInferior = document.querySelector("#palo-inferior");
+    paloInferior.innerHTML = palosRandom;
+
+    let valorCarta = document.querySelector(".valor-carta");
+    valorCarta.innerHTML = valoresRandom;
+  };
+
+  console.log(createCard());
 };
